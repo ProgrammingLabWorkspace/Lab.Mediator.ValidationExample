8cf725e3eaf446200e891e89d06783cb28cd8ef9
Christian Oliveira <christianoliveira1908@hotmail.com>
2025-08-16 12:47:31 -0300

    Criado controller e dto para cadastro de usuário.

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Lab.Mediator.ValidationExample.Core.DTO.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.Mediator.ValidationExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Create([FromBody] WriteUserDto writeUser)
+        {
+            return Created();
+        }
+    }
+}
---
+++ Lab.Mediator.ValidationExample.Core/DTO/User/WriteUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab.Mediator.ValidationExample.Core.DTO.User
+{
+    public class WriteUserDto
+    {
+        public string? Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }        
+    }
+}
